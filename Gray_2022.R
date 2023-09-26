629586d0a5ed9c0af61656dd2f607afa5bd13719
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-09-26 13:46:49 -0500

    Added prepared hypoxia data set.

--- Gray_2022.R
+++ Gray_2022.R
@@ -3,6 +3,7 @@ library(conflicted)
 library(GenomicFeatures)
 library(DESeq2)
 library(ggrepel)
+library(tools)
 conflicts_prefer(dplyr::select)
 conflicts_prefer(dplyr::rename)
 conflicts_prefer(dplyr::filter)
@@ -37,8 +38,9 @@ class(colData) <- "data.frame"
 #                                 ~Line + Treatment + Line:Treatment ) %>%
 #        DESeq(.) %>%
 #        estimateSizeFactors(.)
-save(list = c(),
-     file = "")
+save(list = c("counts","samples"),
+     file = "hypoxia.RData")
+resaveRdaFiles("hypoxia.RData")
 wt <- which(colData$Line == "WT")
 ko <- which(colData$Line == "KO")
 colData_wt <- colData[wt,]
@@ -55,7 +57,11 @@ res_ko <- results(deseq_ko,alpha = 0.05,
                   contrast = list("Treatment_Hypoxia_vs_Normoxia"))
 #res_wt<-lfcShrink(deseq_wt,coef="Treatment_Hypoxia_vs_Normoxia",type="ashr")
 #res_ko<-lfcShrink(deseq_ko,coef="Treatment_Hypoxia_vs_Normoxia",type="ashr")
+pdat <- data.frame(wt = res_wt$log2FoldChange,
+                   ko = res_ko$log2FoldChange)
+library(cowplot)
+rows <- with(pdat,which(abs(wt) > 1 | abs(ko) > 1))
 plot(res_wt$log2FoldChange,res_ko$log2FoldChange,pch = 20)
-coef(lm(res_ko$log2FoldChange ~ res_wt$log2FoldChange))
-abline(a = -0.03316,b = 0.50728,col = "magenta",lty = "dashed")
+coef(lm(res_ko$log2FoldChange[rows] ~ res_wt$log2FoldChange[rows]))
+abline(a = -0.1326,b = 0.4986,col = "magenta",lty = "dashed")
 subset(res_wt,log2FoldChange > 10)
